de1676396a9a1701d089d8c80a738a50ea0f75e6
Brian Mendez <brmendez@gmail.com>
2014-11-24 13:43:43 -0800

    Initial Commit

---
+++ BurgerMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BurgerMenu
+//
+//  Created by Brian Mendez on 11/24/14.
+//  Copyright (c) 2014 Brian Mendez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BurgerMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BurgerMenu
+//
+//  Created by Brian Mendez on 11/24/14.
+//  Copyright (c) 2014 Brian Mendez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
